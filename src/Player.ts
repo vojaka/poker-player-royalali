f733f9558de72506abbc8af32bbcafbed1a6a097
Marco Lehmann <m99@posteo.de>
2022-08-03 21:38:18 +0200

    clean-up

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,13 @@
+import { GameState } from './GameState'
+
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+  public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
+    betCallback(0)
   }
 
-  public showdown(gameState: any): void {
+  public showdown(gameState: GameState): void {
 
   }
-};
+}
 
-export default Player;
+export default Player
