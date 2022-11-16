d17a753b8b6b78a5d56cefee92207abff637f539
KristjanLõhmus <lohmus@rushstreetinteractive.com>
2022-11-16 11:52:19 +0200

    NOT PYTHON

--- src/Evaluator.ts
+++ src/Evaluator.ts
@@ -3,8 +3,9 @@ import {Card} from "./interface/Card";
 export class Evaluator {
 
     public evaluate(cards_in_hand: Card[]): number {
-        if (cards_in_hand[0].rank === cards_in_hand[1].rank):
+        if (cards_in_hand[0].rank === cards_in_hand[1].rank) {
             return 1
+        }
         return Math.random()
     }
 
