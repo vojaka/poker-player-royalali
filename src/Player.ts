e7036db0bfef894962cf77ced9f052763964cd92
Brait Õispuu <oispuu@rushstreetinteractive.com>
2022-11-16 11:57:11 +0200

    Only call if pair

--- src/Player.ts
+++ src/Player.ts
@@ -1,10 +1,19 @@
+import { Evaluator } from "./Evaluator";
 import { GameState } from "./interface/GameState";
 
 export class Player {
     public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
 
+    const fold = 0;
     const call = gameState.current_buy_in - gameState.players[gameState.in_action]["bet"];
-    betCallback(call);
+
+    const evaluator = new Evaluator();
+    let action = fold;
+    if (evaluator.evaluate(gameState.players[gameState.in_action].hole_cards) > 0.9) {
+      action = call;
+    }
+
+    betCallback(action);
   }
 
   public showdown(gameState: any): void {
