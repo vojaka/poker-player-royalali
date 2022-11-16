d8deb304ad9708eccb0b2e05802317b6c5ab4998
Brait Õispuu <oispuu@rushstreetinteractive.com>
2022-11-16 14:18:58 +0200

    test raise

--- src/Player.ts
+++ src/Player.ts
@@ -14,7 +14,7 @@ export class Player {
 
   calculateAction(action: Actions, gameState: GameState) {
     const currentBetAmount = gameState.players[gameState.in_action]["bet"];
-    switch(action) {
+    switch (action) {
       case Actions.FOLD:
         return 0;
       case Actions.CALL:
@@ -26,17 +26,45 @@ export class Player {
     }
   }
 
+  public checkCommunityCards = (gameState) => {
+    // We have a pair for now
+    const cards_in_hand = gameState.players[gameState.in_action].hole_cards;
+    if (cards_in_hand[0].rank === cards_in_hand[1].rank) {
+      const ourRank = cards_in_hand[0].rank;
+      const filtered = gameState.community_cards.filter((elem) => {
+        return elem.rank === ourRank;
+      })
+      if (filtered.length > 0) {
+        return Actions.RAISE;
+      } else {
+        return Actions.CALL;
+      }
+    }
+    return Actions.CALL;
+    // const allCards = [...gameState.players[gameState.in_action], ...gameState.community_cards];
+    // // Check for 3 of a kind
+    // allCards.reduce((a, b) => {
+
+    // })
+  }
+
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     const evaluator = new Evaluator();
     const random = Math.random();
     const currentBetAmount = gameState.players[gameState.in_action]["bet"];
+
     let action = Actions.FOLD;
-    if (evaluator.evaluate(gameState.players[gameState.in_action].hole_cards) === 1) {
-      action = Actions.CALL;
-    } else if (currentBetAmount === 0) { // We don't have small or big blind
-      if (random > 0.9) {
+
+    if (!gameState.community_cards.length) {
+      if (evaluator.evaluate(gameState.players[gameState.in_action].hole_cards) === 1) {
         action = Actions.CALL;
+      } else if (currentBetAmount === 0) { // We don't have small or big blind
+        if (random > 0.9) {
+          action = Actions.CALL;
+        }
       }
+    } else {
+      action = this.checkCommunityCards(gameState);
     }
 
     betCallback(this.calculateAction(action, gameState));
