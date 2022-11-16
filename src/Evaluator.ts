27d5dc08c51ba6325499651bfe46f6705127f8aa
KristjanLõhmus <lohmus@rushstreetinteractive.com>
2022-11-16 13:57:42 +0200

    Ranking api request

--- src/Evaluator.ts
+++ src/Evaluator.ts
@@ -1,13 +1,30 @@
-import { Card } from "./interface/Card";
-
+import {Card} from "./interface/Card";
+import * as express from 'express';
 export class Evaluator {
+    private app = express();
 
     public evaluate(cards_in_hand: Card[]): number {
         if (cards_in_hand[0].rank === cards_in_hand[1].rank) {
             return 1
-        } else {
-            return 0
         }
+        let rankings: any;
+        this.getHandRank(cards_in_hand).then(response =>
+            rankings = response)
+        console.log(rankings)
+        return Math.random()
     }
 
-}
+    public getHandRank(cards: Card[]): any {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(cards)
+        };
+        return fetch('http://rainman.leanpoker.org/rank', requestOptions)
+            .then((response) => response.json())
+            .then((responseData) => {
+                return responseData
+            }).catch(error => console.warn(error))
+    }
+
+    }
