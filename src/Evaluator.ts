c2566e966f95d7c4f79b4d7c01736480a607676c
Risko Ruus <ruus@rushstreetinteractive.com>
2022-11-16 13:36:39 +0200

    Folding when current bet is 0 and we don't have a pair

--- src/Evaluator.ts
+++ src/Evaluator.ts
@@ -1,12 +1,13 @@
-import {Card} from "./interface/Card";
+import { Card } from "./interface/Card";
 
 export class Evaluator {
 
     public evaluate(cards_in_hand: Card[]): number {
         if (cards_in_hand[0].rank === cards_in_hand[1].rank) {
             return 1
+        } else {
+            return 0
         }
-        return Math.random()
     }
 
 }
--- src/Player.ts
+++ src/Player.ts
@@ -2,15 +2,21 @@ import { Evaluator } from "./Evaluator";
 import { GameState } from "./interface/GameState";
 
 export class Player {
-    public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
+  public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
 
     const fold = 0;
-    const call = gameState.current_buy_in - gameState.players[gameState.in_action]["bet"];
-
+    const currentBetAmount = gameState.players[gameState.in_action]["bet"];
+    const call = gameState.current_buy_in - currentBetAmount;
     const evaluator = new Evaluator();
+    const random = Math.random();
+
     let action = fold;
-    if (evaluator.evaluate(gameState.players[gameState.in_action].hole_cards) > 0.9) {
+    if (evaluator.evaluate(gameState.players[gameState.in_action].hole_cards) === 1) {
       action = call;
+    } else if (currentBetAmount === 0) { // We don't have small or big blind
+      if (random > 0.9) {
+        action = call;
+      }
     }
 
     betCallback(action);
